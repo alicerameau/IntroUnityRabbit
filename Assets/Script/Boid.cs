a37e22f7bcced9d422370d4fe14cdf3c2923ec19
Alice Rameau <alice.rameau60@gmail.com>
2020-10-22 15:23:40 +0200

    End exercise IntroductionAuScripting
    plus creation prefab "Rabbit" in Assets in order to include it in the scene UnCoinDeParadis

--- Assets/Script/Boid.cs
+++ Assets/Script/Boid.cs
@@ -7,14 +7,18 @@ public class Boid : MonoBehaviour
     public float zoneRepulsion = 5;
     public float zoneAlignement = 7;
     public float zoneAttraction = 50;
+    public float Sol = 0;
 
     public float forceRepulsion = 30;
     public float forceAlignement = 30;
     public float forceAttraction = 30;
+    public float forceDecolle = 100;
 
     public Vector3 target = new Vector3();
     public float forceTarget = 20;
     public bool goToTarget = false;
+    public bool stopToTarget = false;
+    private bool atTarget = false;
 
     public Vector3 velocity = new Vector3();
     public float maxSpeed = 20;
@@ -81,15 +85,31 @@ public class Boid : MonoBehaviour
         //On fait la moyenne des forces, ce qui nous rend indépendant du nombre de boids
         sumForces /= nbForcesApplied;
 
+        //Restent éloignés du sol
+        float distSol = Mathf.Max(0, transform.position.y - Sol);
+        if (distSol < 5)
+        {
+            float forceRejet = Mathf.Pow(1.0f - (distSol / 5), 2) * forceDecolle;
+            sumForces += new Vector3(0, forceDecolle, 0);
+        }
+
         //Si on a une target, on l'ajoute
+        float distToTarget = 0;
         if (goToTarget)
         {
             Vector3 vecToTarget = target - transform.position;
-            if (vecToTarget.sqrMagnitude < 1)
+            distToTarget = vecToTarget.magnitude;
+            if (distToTarget < 0.5f)
+            {
                 goToTarget = false;
+                atTarget = true;
+            }
             else
             {
+                atTarget = false;
                 Vector3 forceToTarget = vecToTarget.normalized * forceTarget;
+                if (distToTarget < 5 && stopToTarget)
+                    forceToTarget *= 10;
                 sumForces += forceToTarget;
                 colorDebugForce += Color.magenta;
                 nbForcesApplied++;
@@ -113,6 +133,11 @@ public class Boid : MonoBehaviour
             velocity = velocity.normalized * maxSpeed;
         if (velocity.sqrMagnitude < minSpeed * minSpeed)
             velocity = velocity.normalized * minSpeed;
+
+        //Si on doit freiner sur la cible, on limite la vitesse
+        if ((goToTarget || atTarget) && stopToTarget)
+            if (distToTarget < 3)
+                velocity = velocity.normalized * distToTarget / 10.0f;
 
         //On regarde dans la bonne direction        
         if (velocity.sqrMagnitude > 0)
--- Assets/Script/BoidManager.cs
+++ Assets/Script/BoidManager.cs
@@ -24,11 +24,21 @@ public class BoidManager : MonoBehaviour
     public float startSpread = 10;
 
     public float maxDistBoids = 30;
+    public float Sol = 0;
 
     public float periodRetargetBoids = 6;
     public float periodNoTargetBoids = 3;
+    public float periodLandedBoids = 6;
     private float timerRetargetBoids = 0;
-    private bool setTargetToBoids = true;
+    private enum StateBoids
+    {
+        RANDOM_FLIGHT,
+        HAS_TARGET,
+        LANDING
+    }
+    private StateBoids nextStateBoids = StateBoids.RANDOM_FLIGHT;
+
+    public bool setBirdView = true;
 
     private List<Boid> boids = new List<Boid>();
     public ReadOnlyCollection<Boid> roBoids
@@ -53,28 +63,81 @@ public class BoidManager : MonoBehaviour
 
     void Update()
     {
-        //Décrémente la temporisation
+
+        if (setBirdView)
+        {
+
+            Camera.main.transform.parent = boids[0].transform;
+            Camera.main.transform.rotation = boids[0].transform.rotation;
+            Camera.main.transform.localPosition = new Vector3();
+            boids[0].GetComponentInChildren<Renderer>().enabled = false;
+        }
+        else
+        {
+            Camera.main.transform.parent = null;
+            boids[0].GetComponentInChildren<Renderer>().enabled = true;
+        }
+
         timerRetargetBoids -= Time.deltaTime;
         if (timerRetargetBoids <= 0)
         {
-            if (!setTargetToBoids)
-                timerRetargetBoids = periodNoTargetBoids;
-            else
-                timerRetargetBoids = periodRetargetBoids;
-
             Vector3 target = Random.insideUnitSphere * maxDistBoids;
             target.y = Mathf.Max(Mathf.Abs(target.y), 10);
+
+            switch (nextStateBoids)
+            {
+                case StateBoids.HAS_TARGET:
+                    timerRetargetBoids = periodRetargetBoids;
+                    break;
+                case StateBoids.RANDOM_FLIGHT:
+                    timerRetargetBoids = periodNoTargetBoids;
+                    break;
+                case StateBoids.LANDING:
+                    timerRetargetBoids = periodLandedBoids;
+                    break;
+            }
+
             foreach (Boid b in boids)
             {
-                b.goToTarget = false;
-                if (setTargetToBoids && Random.Range(0.0f, 1.0f) < 0.3f)
+                b.Sol = Sol;
+
+                switch (nextStateBoids)
                 {
-                    b.target = target;
-                    b.goToTarget = true;
+                    case StateBoids.HAS_TARGET:
+                        b.goToTarget = false;
+                        b.stopToTarget = false;
+                        if (Random.Range(0.0f, 1.0f) < 0.3f)
+                        {
+                            b.target = target;
+                            b.goToTarget = true;
+                        }
+                        break;
+                    case StateBoids.RANDOM_FLIGHT:
+                        b.goToTarget = false;
+                        b.stopToTarget = false;
+                        break;
+
+                    case StateBoids.LANDING:
+                        b.target = b.transform.position;
+                        b.target.y = Sol;
+                        b.stopToTarget = true;
+                        b.goToTarget = true;
+                        break;
                 }
             }
 
-            setTargetToBoids = !setTargetToBoids;
+            switch (nextStateBoids)
+            {
+                case StateBoids.HAS_TARGET:
+                    nextStateBoids = StateBoids.RANDOM_FLIGHT;
+                    break;
+                case StateBoids.RANDOM_FLIGHT:
+                    nextStateBoids = StateBoids.LANDING;
+                    break;
+                case StateBoids.LANDING:
+                    nextStateBoids = StateBoids.HAS_TARGET;
+                    break;
+            }
         }
     }
 }
